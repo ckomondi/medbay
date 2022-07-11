9ee78ca0e3db29bce1dfedaabe55569a6f9f6728
Carl Omondi <carlkomondi@gmail.com>
2022-07-11 17:22:11 -0500

    Rebased project

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+
+
+
+
+
+import type { AppProps } from 'next/app'
+
+function App({ Component, pageProps } : AppProps) {
+
+    return <Component {...pageProps} />
+
+}
+
+export default App; 
+
+
+
+
+
+
---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+function Home() {
+
+    return (
+
+        <h1>Home Page</h1>
+    )
+
+}
+
+
+export default Home; 
+
+
+
+
+
+
